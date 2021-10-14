eee9ee565f8a82589b01fa102dcce93f3a47a71b
Vulpeshprajan <vulpeashprajan@gmail.com>
2021-10-15 00:00:22 +1100

    mongo connected

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 
-const app = express()
+const app = express();
 
 import helmet from "helmet";
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
+
+// connect mongoDb
+import mongoClient from "./src/config/db.js";
+mongoClient(); 
 
 // middlewares
 app.use(helmet());
 
-
-
-
-
-
-
 app.use("/", (req, res) => {
-    res.json({ message: "hello world" })
-
+  res.json({ message: "hello world" });
 });
 
-
-
 app.listen(PORT, (error) => {
-    if (error) {
-        return console.log(error)
+  if (error) {
+    return console.log(error);
+  }
 
-    }
-
-    console.log(`Server is running at http://localhost:${PORT}`)
-
-
-})
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
---
+++ src/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const mongoClient = async () => {
+    try {
+        if(!process.env.MONGO_CLIENT) {
+        console.log("MONGO_CLIENT is not defined, please create MONGO_CLIENT and provide mongoDB connection string")
+    }
+
+
+    const con = await mongoose.connect(process.env.MONGO_CLIENT);
+    if (con) {
+        return console.log("mongoDB is connected");
+    }
+    console.log("fail to connect mongoDB")
+} catch (error) {
+    console.log(error)
+    
+}
+
+}
+
+export default mongoClient;
