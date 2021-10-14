9f4e474a3eb08423e80d3fea86acbbcd8bbff853
Vulpeshprajan <vulpeashprajan@gmail.com>
2021-10-14 23:13:37 +1100

    inital api

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express()
+
+import helmet from "helmet";
+
+const PORT = process.env.PORT || 8000
+
+// middlewares
+app.use(helmet());
+
+
+
+
+
+
+
+app.use("/", (req, res) => {
+    res.json({ message: "hello world" })
+
+});
+
+
+
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log(error)
+
+    }
+
+    console.log(`Server is running at http://localhost:${PORT}`)
+
+
+})
